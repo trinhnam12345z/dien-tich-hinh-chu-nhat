ecb3d975a432523b516c151c1a2f5e08383082e2
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-08 15:23:55 +0700

    Diện tích HCN

---
+++ Diện tích HCN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Diện_tích_HCN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
